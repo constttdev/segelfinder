ed61498112fc1bef2ee68ae2ce6cae763001bab8
= <=>
2024-08-18 17:37:01 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+	colors: {
+		text: '#090e11',
+		background: '#f8fafc',
+		primary: '#5790c1',
+		secondary: '#9bc1e2',
+		accent: '#60a2dc'
+	},
+
+	plugins: []
+};
