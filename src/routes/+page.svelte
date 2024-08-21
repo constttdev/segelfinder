321fa53d54a80a9b46fa22d04b37b0baa506cb90
= <=>
2024-08-21 16:05:06 +0200

    Added tailwind colors and changed some other stuff (idk really know anymore :sob:)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-100 h-100 absolute bg-slate-600"></div>
+<div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="font-inter font-bold text-4xl">Find the right Sails for you</h1>
+    <button class="mt-5 px-8 py-4 rounded-lg bg-primary text-white font-inter uppercase font-semibold tracking-wider">Find Sails</button>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,18 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
-	},
-	colors: {
-		text: '#090e11',
-		background: '#f8fafc',
-		primary: '#5790c1',
-		secondary: '#9bc1e2',
-		accent: '#60a2dc'
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif']
+			},
+			colors: {
+				text: '#090e11',
+				background: '#f8fafc',
+				primary: '#5790c1',
+				secondary: '#9bc1e2',
+				accent: '#60a2dc'
+			}
+		}
 	},
 
 	plugins: []
